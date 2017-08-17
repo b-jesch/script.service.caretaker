439686b4e733acf5329577f476cf50483f2627b6
Birger Jesch <birger.jesch@gmail.com>
2017-08-17 20:57:12 +0200

    [script.service.caretaker] v0.0.2

--- service.py
+++ service.py
@@ -40,7 +40,7 @@ def service():
             xbmc.log('[%s %s] No potentially harmful repositories found' % (__addonid__, __addonversion__), xbmc.LOGNOTICE)
         else:
             for hit in hits:
-                xbmc.log('[%s %s] Potentially harmful repository found: \'%s\' (%s)' % (__addonid__, __addonversion__, hit.get('name', ''), hit.get('addonid', '')), xbmc.LOGNOTICE)
+                xbmc.log('[%s %s] Potentially harmful repository \'%s\' (%s) found' % (__addonid__, __addonversion__, hit.get('name', ''), hit.get('addonid', '')), xbmc.LOGNOTICE)
             xbmcgui.Dialog().notification(__LS__(30011), __LS__(30012), xbmcgui.NOTIFICATION_WARNING)
     else:
         xbmc.log('[%s %s] Could not execute JSON query' % (__addonid__, __addonversion__), xbmc.LOGFATAL)
